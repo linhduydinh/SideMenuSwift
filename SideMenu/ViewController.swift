46a9ba4367295e6fcd0d373e5c2fe90611011500
Tyler Dinh <linhduydinh@gmail.com>
2018-11-18 21:32:23 +0700

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by MAC on 11/18/18.
+//  Copyright © 2018 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
